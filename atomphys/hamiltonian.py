80674331ba0bea5e6d14669eb187cd020f636b09
Will Cairncross <will@atom-computing.com>
2021-08-31 19:20:36 -0700

    move sublevels from hamiltonian.py to new file

--- atomphys/hamiltonian.py
+++ atomphys/hamiltonian.py
@@ -1,21 +1,15 @@
 import qutip as qt
 
+try:
+    from . import _ureg, _HAS_PINT
+except ImportError:
+    _HAS_PINT = False
+    _ureg = None
+
 class Hamiltonian(qt.qobj.Qobj):
     def __init__(self, *args, parent=None, **kwargs):
         super().__init__(*args, **kwargs)
         self._parent = parent
-
-class Qnums(dict):
-    def __init__(self, **state):
-        if state.coupling == 'LS':
-            state.L
-            state.S
-            state.J
-        else:
-            pass
-
-        super(Qnums, self).__init__(
-            {'energy': energy, 'configuration': configuration, 'J': J,  **term})
 
 class Ops(dict):
     def __init__(self, **state):
@@ -25,10 +19,3 @@ class Ops(dict):
             state.J
         else:
             pass
-
-class Sublevel(dict):
-    def __init__(self):
-        super(Sublevel, self).__init__({})
-
-
-
