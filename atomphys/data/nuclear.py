8800420cefaebf7276660beca11b06cb18f86aa5
Will Cairncross <will@atom-computing.com>
2021-09-09 11:43:02 -0600

    consolidate methods for loading nuclear data into nuclear.py

--- atomphys/data/nuclear.py
+++ atomphys/data/nuclear.py
@@ -6,11 +6,13 @@ import re
 
 current_file = os.path.realpath(__file__)
 directory = os.path.dirname(current_file)
-nuc_table_filename = os.path.join(directory, "NuclearDataJSON.json")
-with open(nuc_table_filename) as f:
-    nuc_table = json.load(f)
+raw_nuc_table_file = os.path.join(directory, "raw_NuclearDataJSON.json")
+nuc_table_file = os.path.join(directory, "NuclearDataJSON.json")
 
 def fetch_isotopes(name, symbol):
+    with open(nuc_table_file) as f:
+        nuc_table = json.load(f)
+
     url = 'https://en.wikipedia.org/wiki/Isotopes_of_' + name.lower()
     with urllib.request.urlopen(url) as response:
         response = response.read()
@@ -90,3 +92,43 @@ def fetch_isotopes(name, symbol):
             data_out[row].update( {"mag_moment_μN": '0.0'})
 
     return data_out
+
+def massage_nuclear_data():
+    with open(raw_nuc_table_file) as f:
+        NuclearDataJSON = json.load(f)
+
+    header_row = NuclearDataJSON[0]['data'][0]
+    col_headers = []
+    for col in header_row:
+        col_headers.append(col['text'])
+
+    table_data = []
+    p = 0
+    for page in NuclearDataJSON:
+        r = 0
+        page['data'].pop(0)
+        for row in page['data']:
+            this_row_data = {key: None for key in col_headers}
+            c = 0
+            for col in row:
+                try:
+                    this_row_data[col_headers[c]] = col['text']
+                except Exception:
+                    pass
+                c = c + 1
+            if not (all([len(item) == 0 for item in this_row_data.values()])):
+                table_data.append(this_row_data)
+            r = r + 1
+        p = p + 1
+
+    table_data_cut = []
+    for row in table_data:
+        nuc_re = re.search('^(\d+) (\w+) (\d+)', row['Nucleus'])
+        if nuc_re is not None:
+            row['Nucleus'] = nuc_re.group(3) + nuc_re.group(2)
+            row['Z'] = nuc_re.group(1)
+            table_data_cut.append(row)
+
+    output_data_file = os.path.join(directory, "NuclearDataJSON.json")
+    with open(output_data_file, 'w') as f:
+        json.dump(table_data_cut, f, indent=4, ensure_ascii=False)
