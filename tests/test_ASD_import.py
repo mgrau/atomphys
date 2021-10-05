f79ab5a238346b4a27f3d4552b344b70cc17601f
Matt Grau <matt.grau@gmail.com>
2021-10-05 17:00:43 +0200

    create pytest that tries to load all neutrals and singly ionized ions from ASD (up to and including U)

---
+++ tests/test_ASD_import.py
@@ -0,0 +1,21 @@
+# This test can take a very long time to run, and it makes many calls to the NIST ASD
+# to run it use `pytest -m ASD`
+# but beware, it will take several minutes, which is why it is disabled by default
+
+import pytest
+from atomphys import Atom
+from atomphys.data import nist
+from atomphys import symbols
+
+up_to_element = 'U'
+partial_periodic_table = symbols[:symbols.index(up_to_element)+1]
+atoms_ions = partial_periodic_table + [name + '+' for name in partial_periodic_table[1:]]
+    
+@pytest.mark.ASD
+@pytest.mark.parametrize('atom', atoms_ions)
+def test_parse_state(atom):
+    Atom(atom)
+
+
+# failing
+# AS
--- tests/test_states.py
+++ tests/test_states.py
@@ -1,7 +1,4 @@
 from atomphys import Atom
 
 
-def test_Mg():
-    Mg = Atom('Mg+')
 
-    assert Mg.states[0].term == '2S1/2'
