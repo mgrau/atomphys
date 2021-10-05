cf4bc7e8e66bf9c2623064fbf88ea5d6be38cb73
Matt Grau <matt.grau@gmail.com>
2021-10-05 13:07:24 +0200

    change default print format for pint

--- atomphys/__init__.py
+++ atomphys/__init__.py
@@ -1,7 +1,7 @@
 try:
     from pint import UnitRegistry
     _ureg = UnitRegistry(system='atomic', auto_reduce_dimensions=True)
-    _ureg.default_format = '~P'
+    _ureg.default_format = '~0.3gP'
     _HAS_PINT = True
 except ImportError:
     _HAS_PINT = False
