665dbde1f2525ef4dee413fe13cfb13f4d361947
Matt Grau <matt.grau@gmail.com>
2021-10-15 13:06:38 +0200

    add docstrings to util.py

--- atomphys/util.py
+++ atomphys/util.py
@@ -1,9 +1,11 @@
 import re
+from typing import Callable
 
 re_energy = re.compile("-?\\d+\\.\\d*|$")
 
 
-def sanitize_energy(s):
+def sanitize_energy(s: str) -> str:
+    """sanitize energy strings from NIST ASD by removing annotations"""
     # return re_energy.findall(s)[0]
 
     # this is about 3.5× faster than re.findall, but it's less flexible
@@ -11,24 +13,22 @@ def sanitize_energy(s):
     return s.strip("()[]aluxyz +?").replace("&dagger;", "")
 
 
-def fsolve(func, x0, x1=None, tol=1.49012e-08, maxfev=100):
+def fsolve(func: Callable, x0, x1=None, tol: float = 1.49012e-08, maxfev: int = 100):
     """
     Find the roots of a function.
 
-    Return the roots of the equation ``func(x) = 0`` given a
-    starting estimate x0
+    Return the roots of the equation `func(x) = 0` given a
+    starting estimate `x0`. A second starting estimate `x1` can be
+    used to bound a root if `f(x)` has multiple roots.
 
-    Parameters
-    ----------
-    func : callable ``f(x, *args)``
-        a function that takes a single argument
-    x0: numeric
-        The estarting estimate for the roots of ``func(x) = 0``
-    tol: float, optional
-        The calculation will terminate if the relative error between two
-        consecutive iterates is at most `tol`
-    maxfev: int, optional
-        The maximum number of calls to the function.
+    Arguments:
+        func: a function `f(x)` that takes a single argument `x`
+        x0: The starting estimate for the roots of `func(x) = 0``
+        x1: A secton starting estimate that together with `x1` bounds
+            the root. Defaults to `1.0001 * x0`.
+        tol: The calculation will terminate if the relative
+            error between two consecutive iterates is at most `tol`
+        maxfev: The maximum number of calls to the function.
     """
     if x1 is None:
         x1 = x0 * 1.001
